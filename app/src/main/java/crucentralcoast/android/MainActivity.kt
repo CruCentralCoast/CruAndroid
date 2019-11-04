84b4d12123d8b409422ee59c97452706a52bbd90
Carter Moody <Cartman2490@gmail.com>
2019-11-03 17:05:18 -0800

    Added Navigation Drawer Menu Functionality. Buttons do not work to launch fragments or activities Added placeholder vector files

--- app/src/main/java/crucentralcoast/android/MainActivity.kt
+++ app/src/main/java/crucentralcoast/android/MainActivity.kt
@@ -1,46 +1,78 @@
 package crucentralcoast.android
 
-import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var textMessage: TextView
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_events -> {
-                textMessage.setText("Events")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_community -> {
-                textMessage.setText("Community")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_ministry -> {
-                textMessage.setText("Ministry")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_missions -> {
-                textMessage.setText("Missions")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_resources -> {
-                textMessage.setText("Resources")
-                return@OnNavigationItemSelectedListener true
-            }
 
-        }
-        false
-    }
-
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_events -> {
+                Toast.makeText(this, "Events clicked", Toast.LENGTH_SHORT).show()
+                //setContentView(R.layout.activity_events)
+            }
+            R.id.nav_community -> {
+                Toast.makeText(this, "Community clicked", Toast.LENGTH_SHORT).show()
+                setContentView(R.layout.community)
+            }
+            R.id.nav_ministry -> {
+                Toast.makeText(this, "Ministry clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_missions -> {
+                Toast.makeText(this, "Missions clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_resources -> {
+                Toast.makeText(this, "Resources clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_update -> {
+                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_logout -> {
+                Toast.makeText(this, "Logout clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
 }
